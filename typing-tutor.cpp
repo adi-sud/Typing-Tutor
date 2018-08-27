4a43ed1dc2739d3ebd1f0ba97ad1708ccd397697
Aditya Sud <adisud007@gmail.com>
2018-08-27 21:25:59 +0530

    Typing tip function

--- typing-tutor.cpp
+++ typing-tutor.cpp
@@ -63,3 +63,41 @@ void main(){
     } // End of Switch case block.
   } // End of While block.
 } // End of main.
+
+// Function for displaying typing tips.
+
+void tips(void){
+  clrscr();
+  gotoxy(2, 3);
+  cout << "Typing tips";
+
+  gotoxy(2, 4);
+  cout << "-----------";
+
+  gotoxy(2, 6);
+  cout << "1. First and formost make sure that you are seated comfortably with the ";
+  gotoxy(5, 7);
+  cout << "keyboard placed at appropriate height."
+
+  gotoxy(2, 9);
+  cout << "2. Use all your fingers while typing, with the thumb being used for pressing ";
+  gotoxy(5, 10);
+  cout << "the space bar.";
+
+  gotoxy(2, 12);
+  cout << "3. Explore the various keys present on the keyboard and gradually try to build ";
+  gotoxy(5, 13);
+  cout << "your finger memory.";
+
+  gotoxy(2, 15);
+  cout << "4. Avoid looking at the keybord while typing and try to keep your eyes on the "
+  gotoxy(5, 16);
+  cout << "screen";
+
+  gotoxy(2, 18);
+  cout << "5. Practice as much as you can to build your typing skills."
+
+  gotoxy(30, 30);
+  cout << "Press any key to go back..";
+  getch();
+}
